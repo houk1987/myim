b1decc57849ba2ee92d7a4969ec846222360db53
hk <houk1987@gmail.com>
2022-08-18 07:14:37 +0800

    im-server 实现了人员注册功能

--- im-server/src/main/java/com/myim/server/service/smack/Configuration.java
+++ im-server/src/main/java/com/myim/server/service/smack/Configuration.java
@@ -3,18 +3,22 @@ package com.myim.server.service.smack;
 import org.jivesoftware.smack.*;
 import org.jivesoftware.smack.tcp.XMPPTCPConnection;
 import org.jivesoftware.smack.tcp.XMPPTCPConnectionConfiguration;
+import org.jivesoftware.smackx.iqregister.AccountManager;
+import org.jxmpp.jid.parts.Localpart;
 import org.jxmpp.stringprep.XmppStringprepException;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 @Component
 public class Configuration {
 
-    private String serverName="192.168.101.20";
+    private String serverName="h7.maxlink.top";
 
-    private int port=5222;
+    private int port=11008;
 
     public XMPPTCPConnectionConfiguration getXMPPTCPConnectionConfiguration() throws XmppStringprepException {
         XMPPTCPConnectionConfiguration.Builder builder = XMPPTCPConnectionConfiguration.builder();
@@ -47,12 +51,28 @@ public class Configuration {
         this.port = port;
     }
 
+
+
     public static void main(String[] args) {
         Configuration configuration = new Configuration();
         try {
             XMPPTCPConnection connection = new XMPPTCPConnection(configuration.getXMPPTCPConnectionConfiguration());
             connection.connect();
             connection.login("test","123");
+            connection.login("test3","123");
+//            AccountManager.sensitiveOperationOverInsecureConnectionDefault(true);
+//            AccountManager accountManager = AccountManager.getInstance(connection);
+
+//            Map<String, String> map = new HashMap<String, String>();
+//            String usr="test3";
+//            String pwd="hkqaz123";
+//            String email ="houk@gmail.com";
+//            map.put("username", usr);
+//            map.put("name", usr);
+//            map.put("password", pwd);
+//            map.put("email", email);
+//            accountManager.createAccount(Localpart.fromOrThrowUnchecked(usr),"123",map);
+
             while (true){
 
             }
